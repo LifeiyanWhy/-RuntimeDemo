617ef8af372b1340c4a10a42f0bcf88578c853fd
LifeiyanWhy <1553652636@qq.com>
2018-10-22 16:17:01 +0800

    Initial Commit

---
+++ RuntimeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeTest
+//
+//  Created by 李飞艳 on 2018/10/22.
+//  Copyright © 2018年 李飞艳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RuntimeTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RuntimeTest
+//
+//  Created by 李飞艳 on 2018/10/22.
+//  Copyright © 2018年 李飞艳. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
